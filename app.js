02da6df7faee126c9dc6e0b39eea0b790c67c9a9
Zaki Mulla <zakim2307@gmail.com>
2021-04-07 11:50:27 +0530

    error not resolved yet

--- app.js
+++ app.js
@@ -64,15 +64,15 @@ app.delete("/items/:id", (req, res) => {
 });
 
 app.put("/items/:id", (req, res) => {
-    const id = req.params.id;
-    Item.findByIdAndUpdate(id, req.body)
-      .then((result) => {
-        res.json({ msg: "Updated S" });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+  const id = req.params.id;
+  Item.findByIdAndUpdate(id, req.body)
+    .then((result) => {
+      res.json({ msg: "Updated Succesfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use((req, res) => {
   res.render("error");
